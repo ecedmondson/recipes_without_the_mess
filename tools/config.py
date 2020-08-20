a72a352210a8626ca39a8d977a14b5af23d0188a
Emily Edmondson <ecedmondson@gmail.com>
2020-08-20 12:57:34 -0500

    Recipes DD: add dimitras dishes to config

--- tools/config.py
+++ tools/config.py
@@ -2,6 +2,7 @@ import argparse
 from ba.ba import BonAppetit
 from lc.lc import LeitesCulinaria
 from f52.f52 import Food52
+from dd.dd import DimitrasDishes
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -10,5 +11,5 @@ def get_parser():
     group.add_argument("--bon-appetit", "-ba", action="store_const", dest="action", const=BonAppetit,)
     group.add_argument("--leites-culinaria", "-lc", action="store_const", dest="action", const=LeitesCulinaria,)
     group.add_argument("--food52", "-f52", action="store_const", dest="action", const=Food52,)
-
+    group.add_argument("--dimitras-dishes", "-dd", action="store_const", dest="action", const=DimitrasDishes,)
     return parser
