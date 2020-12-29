ece52184fc8ee6679f816284b3660463567e2cf7
Emily Edmondson <ecedmondson@gmail.com>
2020-12-29 12:24:14 -0600

    Update config.py

--- tools/config.py
+++ tools/config.py
@@ -4,6 +4,7 @@ from lc.lc import LeitesCulinaria
 from f52.f52 import Food52
 from dd.dd import DimitrasDishes
 from nyt.nyt import NYT
+from osg.osg import OhSheGlows
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -13,5 +14,6 @@ def get_parser():
     group.add_argument("--leites-culinaria", "-lc", action="store_const", dest="action", const=LeitesCulinaria,)
     group.add_argument("--food52", "-f52", action="store_const", dest="action", const=Food52,)
     group.add_argument("--dimitras-dishes", "-dd", action="store_const", dest="action", const=DimitrasDishes,)
-    group.add_argument("--nyt-cooking", "-nyt", actions="store_const", dest="action", const=NYT,)
+    group.add_argument("--nyt-cooking", "-nyt", action="store_const", dest="action", const=NYT,)
+    group.add_argument("--oh-she-glows", "-osh", action="store_const", dest="action", const=OSG,)
     return parser
